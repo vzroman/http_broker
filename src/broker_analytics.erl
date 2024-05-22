f8037082e9ba17ada0e815ee24c30cb426011336
VAfoninf <vladimir.afonin@faceplate.io>
2024-05-22 17:48:25 +0500

    Count of targets pairs in queue introduced

--- src/broker_analytics.erl
+++ src/broker_analytics.erl
@@ -5,6 +5,10 @@
 -export([start/0, collect_data/0]).
 
 
+-define(DECODE_KEY(K), sext:decode(K) ).
+-define(DECODE_VALUE(V), binary_to_term(V) ).
+
+
 start() ->
 	schedule(),
 	collect_data()
@@ -27,17 +31,66 @@ collect_data() ->
 
 
 get_queue_stats(QueueDB) ->
+  print_queue_contents(QueueDB),
   QueueCount = count_targets(QueueDB),
   ItemCount = count_queue_items(QueueDB),
   AttemptsCount = get_attempts_count(QueueDB),
   {QueueCount, ItemCount, AttemptsCount}
   .
 
+print_queue_contents({ref, Ref, _RefA, _Params2, _Params, "QUEUE"}) ->
+	io:format("QueueDB ref: ~p~n", [Ref]),
+	io:format("Iterating through the database...~n"),
+	case rocksdb:iterator(Ref, []) of
+	{ok, Itr} ->
+		iterate_print(Itr);
+	{error, Reason} ->
+		io:format("Failed to create iterator: ~p~n", [Reason])
+	end.
+
+iterate_print(Itr) ->
+    case rocksdb:iterator_move(Itr, first) of
+        {ok, K, V} ->
+            print_all_keys({ok, K, V}, Itr, next);
+        {error, _Reason} ->
+            io:format("No keys found in the database.~n")
+    end.
+
+print_all_keys({ok, K, V}, Itr, Next) ->
+    io:format("Key: ~p, Value: ~p~n", [?DECODE_KEY(K), ?DECODE_VALUE(V)]),
+    case rocksdb:iterator_move(Itr, Next) of
+        {ok, K1, V1} ->
+            print_all_keys({ok, K1, V1}, Itr, Next);
+        {error, _Reason} ->
+            io:format("End of iteration.~n")
+    end;
+print_all_keys(_, _, _) ->
+    io:format("No more keys found.~n").
 
 count_targets(QueueDB) ->
-	% Todo: implement count_targets
--1
-  .
+	Query = #{},
+	UserFun = fun({Key, _Value}) ->
+		io:format("Processing Key: ~p~n", [Key]),
+		case Key of
+			%{?TARGET(Endpoint, Service, _Ref)} ->
+			{target, Endpoint, Service, _Ref} ->
+				{true, {Endpoint, Service}};
+			_ ->
+				false
+		end
+	end,
+	GroupedMap = zaya_rocksdb:foldl(QueueDB, Query, fun({Key, Value}, Acc) ->
+		io:format("Key: ~p, Value: ~p~n", [Key, Value]),
+		case UserFun({Key, Value}) of
+			{true, GroupKey} ->
+				io:format("Grouping Key: ~p as ~p~n", [Key, GroupKey]),
+				maps:update_with(GroupKey, fun(List) -> [Value | List] end, [Value], Acc);
+			false ->
+				Acc
+		end
+	end, #{}),
+	io:format("Final GroupedMap: ~p~n", [GroupedMap]),
+	maps:map(fun(_Key, Val) -> length(Val) end, GroupedMap).
 
 count_queue_items(QueueDB) ->
 	Query = #{},
@@ -45,14 +98,18 @@ count_queue_items(QueueDB) ->
     zaya_rocksdb:foldl(QueueDB, Query, UserFun, 0)
 	.
 
-get_attempts_count(QueueDB, Endpoint, Service, Ref, Target) ->
-  case zaya_rocksdb:next(QueueDB, ?TARGET('_','_','_')) of
-	{?TARGET(Endpoint, Service, Ref)=Target, Attempts} ->
-	  Attempts;
-	_ ->
-	  0
-  end
-  .
+get_attempts_count(QueueDB) ->
+	case zaya_rocksdb:last(QueueDB) of
+        {Key, _Value} ->
+            case zaya_rocksdb:read(QueueDB, [Key]) of
+                [{_, Attempts}] when is_integer(Attempts)->
+                    Attempts;
+                _ ->
+                    0
+            end;
+        _ ->
+            0
+    end.
 
 log_data(QueueCount, ItemCount, AttemptsCount) ->
   % log to file with write_file
